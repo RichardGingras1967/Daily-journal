5b47f179c7ce1b1ce3c538d0aaacf2a8f92bf8b9
Richard Gingras <richard.gingras.1@ulaval.ca>
2023-08-17 20:39:32 -0400

    ajouter une session avec express-session

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 const fs = require("fs");
+const cookieParser = require("cookie-parser");
+const sessions = require("express-session");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const MongoStore = require("connect-mongo");
 
 const app = express();
 
@@ -14,16 +20,33 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const connectDB = async ()=> {
-  mongoose.connect(process.env.MONGO_URI, {
+app.use(cookieParser());
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(
+  sessions({
+    secret: process.env.SECRET,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+     // mongoUrl: "mongodb://127.0.0.1:27017/ComposeDB",
+    }),
+  })
+);
+
+const connectDB = async () => {
+    mongoose.connect(process.env.MONGO_URI, {
+    //mongoose.connect("mongodb://127.0.0.1:27017/ComposeDB", {
     useNewUrlParser: true,
   });
-}
-
+};
 
 const postSchema = new mongoose.Schema({
   title: String,
   text: String,
+  author: String,
 });
 
 const listSchema = new mongoose.Schema({
@@ -31,8 +54,14 @@ const listSchema = new mongoose.Schema({
   posts: [postSchema],
 });
 
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
 const Post = mongoose.model("Post", postSchema);
 const List = mongoose.model("List", listSchema);
+const Client = new mongoose.model("Client", userSchema);
 
 const composeText = fs.readFileSync("public/texts/compose.txt", "utf-8");
 const viewText = fs.readFileSync("public/texts/view.txt", "utf-8");
@@ -42,30 +71,96 @@ const aboutContent = fs.readFileSync("public/texts/about.txt", "utf-8");
 const contactContent = fs.readFileSync("public/texts/contact.txt", "utf-8");
 
 app.get("/compose/:categorie", function (req, res) {
-  const categorie = _.capitalize(req.params.categorie);
+  if (req.session.user) {
+    const categorie = _.capitalize(req.params.categorie);
 
-  List.find({ categorie: categorie }).then((items) => {
-    if (items.length === 0) {
-      const greetingPost = new Post({
-        title: "Welcome to your " + categorie + " section",
-        text: "",
-      });
+    List.find({ categorie: categorie }).then((items) => {
+      if (items.length === 0) {
+        const greetingPost = new Post({
+          title: "Welcome to your " + categorie + " section",
+          text: "",
+        });
 
-      const list = new List({
-        categorie: categorie,
-        posts: [greetingPost],
-      });
+        const list = new List({
+          categorie: categorie,
+          posts: [greetingPost],
+        });
 
-      list.save();
-      res.redirect("/compose/" + categorie);
-    } else {
-      res.render("compose.ejs", { categorie: categorie });
-    }
-  });
+        list.save();
+        res.redirect("/compose/" + categorie);
+      } else {
+        res.render("compose.ejs", { categorie: categorie });
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((logged) => {
+    console.log("user logged out");
+  });
+  res.redirect("/");
+});
+
+app.post("/register", (req, res) => {
+  try{
+    const email = req.body.username;
+
+  bcrypt.hash(req.body.password, saltRounds).then((hash) => {
+    const newUser = new Client({
+      email: email,
+      password: hash,
+    });
+    newUser
+      .save()
+      .then((user, err) => {
+        req.session.user = user;
+        res.redirect("/howto");
+      })
+      
+  })
+  .catch((err) => {
+    console.log(err);
+    res.redirect("/login");
+  });
+  }catch(err){
+    console.log(err);
+  }
+  
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.username;
+
+  Client.findOne({ email: email }).then((user) => {
+    if (user != null) {
+      //user exist
+      bcrypt.compare(req.body.password, user.password).then((granted) => {
+        if (granted) {
+          req.session.user = user;
+          res.redirect("/howto");
+        } else {
+          res.redirect("/login");
+        }
+      });
+    } else {
+      res.redirect("/register");
+    }
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -79,11 +174,27 @@ app.get("/contact", (req, res) => {
 app.get("/howto", (req, res) => {
   res.render("howto.ejs", {
     data: [
-      { header: "Compose", text: composeText, bar: "..app/<b>compose</b>/<em>category</em>"},
-      { header: "View", text: viewText, bar: "..app/<b>view</b>/<em>category</em>" },
-      { header: "Read", text: readText, bar: "..app/<b>read</b>/<em>category</em>/<em>post</em>" },
-      { header: "Delete", text: deleteText, bar: "..app/<b>delete</b>/<em>category</em>/<em>post</em>"},
-    ]
+      {
+        header: "Compose",
+        text: composeText,
+        bar: "..app/<b>compose</b>/<em>category</em>",
+      },
+      {
+        header: "View",
+        text: viewText,
+        bar: "..app/<b>view</b>/<em>category</em>",
+      },
+      {
+        header: "Read",
+        text: readText,
+        bar: "..app/<b>read</b>/<em>category</em>/<em>post</em>",
+      },
+      {
+        header: "Delete",
+        text: deleteText,
+        bar: "..app/<b>delete</b>/<em>category</em>/<em>post</em>",
+      },
+    ],
   });
 });
 
@@ -149,19 +260,31 @@ app.get("/delete/:categorie/:id", (req, res) => {
   const id = req.params.id;
   const categorie = req.params.categorie;
 
-  const filter = { categorie: categorie };
+  const search = { categorie: categorie };
+  const filter = { posts: { $elemMatch: { _id: id } } };
   const update = { $pull: { posts: { _id: id } } };
 
-  List.updateOne(filter, update)
-    .then(() => {
-      res.redirect("/view/" + categorie);
+  List.findOne(search, filter)
+    .then((found) => {
+      //only the author of the post can delete it
+      if (found.posts[0].author === req.session.user._id) {
+        List.updateOne(filter, update)
+          .then(() => {
+            res.redirect("/view/" + categorie);
+          })
+          .catch((err) => {
+            res.redirect("/error");
+          });
+      } else {
+        res.send("Unautorise action, you can delete only yours post..");
+      }
     })
     .catch((err) => {
       res.redirect("/error");
     });
 });
 
-app.get("/deleteAll/:categorie", (req, res) => {
+/*app.get("/deleteAll/:categorie", (req, res) => {
   const categorie = req.params.categorie;
 
   const filter = { categorie: categorie };
@@ -173,13 +296,14 @@ app.get("/deleteAll/:categorie", (req, res) => {
     .catch((err) => {
       res.redirect("/error");
     });
-});
+});*/
 
 app.post("/compose/:categorie", (req, res) => {
   const categorie = req.params.categorie;
   const data = {
     title: req.body.blogTitle,
     text: req.body.blogText,
+    author: req.session.user._id.toString(),
   };
 
   const filter = { categorie: categorie };
@@ -200,13 +324,8 @@ app.get("*", (req, res) => {
   });
 });
 
-
-connectDB()
-.then(()=>{
+connectDB().then(() => {
   app.listen(3000, function () {
     console.log(`Server started on port ${PORT}`);
   });
 });
-
-
-
